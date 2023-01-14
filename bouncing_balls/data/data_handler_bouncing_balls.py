2681520eabbfb8317dc7ccfc1c87aa9f0b5f1d84
Shawn Presser <shawnpresser@gmail.com>
2023-01-14 17:45:10 -0600

    Update bouncing balls generator

--- bouncing_balls/data/data_handler_bouncing_balls.py
+++ bouncing_balls/data/data_handler_bouncing_balls.py
@@ -4,15 +4,16 @@ This script comes from the RTRBM code by Ilya Sutskever from
 http://www.cs.utoronto.ca/~ilya/code/2008/RTRBM.tar
 """
 
-from numpy import *                
+from numpy import *
 from scipy import *               
 import pdb
-import cPickle as pickle
+import pickle
 import scipy.io
 
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import tqdm
 
 shape_std=shape
 def shape(A):
@@ -39,29 +40,32 @@ def new_speeds(m1, m2, v1, v2):
 def norm(x): return sqrt((x**2).sum())
 def sigmoid(x):        return 1./(1.+exp(-x))
 
-SIZE=10
+SIZE=8
 # size of bounding box: SIZE X SIZE.
 
-def bounce_n(T=128, n=2, r=None, m=None):
-    if r==None: r=array([1.2]*n)
-    if m==None: m=array([1]*n)
+def bounce_n(T=128, n=2, r=None, m=None, *, res):
+    aspect = (1.0, res[0] / res[1])
+    #size = (aspect[0]*SIZE, aspect[1]*SIZE)
+    size = (SIZE, SIZE)
+    if r is None: r=array([1.2]*n)
+    if m is None: m=array([1]*n)
     # r is to be rather small.
     X=zeros((T, n, 2), dtype='float')
-    v = randn(n,2)
+    v = random.randn(n,2)
     v = v / norm(v)*.5
     good_config=False
     while not good_config:
-        x = 2+rand(n,2)*8
+        x = 2+random.rand(n,2)*8
         good_config=True
         for i in range(n):
             for z in range(2):
-                if x[i][z]-r[i]<0:      good_config=False
-                if x[i][z]+r[i]>SIZE:     good_config=False
+                if x[i][z]-(1/aspect[z])*r[i]<0:       good_config=False
+                if x[i][z]+(1/aspect[z])*r[i]>size[z]: good_config=False
 
         # that's the main part.
         for i in range(n):
             for j in range(i):
-                if norm(x[i]-x[j])<r[i]+r[j]:
+                if norm(aspect*x[i]-aspect*x[j])<r[i]+r[j]:
                     good_config=False
                     
     
@@ -79,15 +83,15 @@ def bounce_n(T=128, n=2, r=None, m=None):
 
             for i in range(n):
                 for z in range(2):
-                    if x[i][z]-r[i]<0:  v[i][z]= abs(v[i][z]) # want positive
-                    if x[i][z]+r[i]>SIZE: v[i][z]=-abs(v[i][z]) # want negative
+                    if x[i][z]-(1/aspect[z])*r[i]<0:       v[i][z]= abs(v[i][z]) # want positive
+                    if x[i][z]+(1/aspect[z])*r[i]>size[z]: v[i][z]=-abs(v[i][z]) # want negative
 
 
             for i in range(n):
                 for j in range(i):
-                    if norm(x[i]-x[j])<r[i]+r[j]:
+                    if norm(aspect*x[i]-aspect*x[j])<r[i]+r[j]:
                         # the bouncing off part:
-                        w    = x[i]-x[j]
+                        w    = aspect*x[i]-aspect*x[j]
                         w    = w / norm(w)
 
                         v_i  = dot(w.transpose(),v[i])
@@ -106,63 +110,75 @@ def ar(x,y,z):
 def matricize(X,res,r=None):
 
     T, n= shape(X)[0:2]
-    if r==None: r=array([1.2]*n)
+    if r is None: r=array([1.2]*n)
 
-    A=zeros((T,res,res), dtype='float')
+    A=zeros((T,res[0],res[1]), dtype='float')
     
-    [I, J]=meshgrid(ar(0,1,1./res)*SIZE, ar(0,1,1./res)*SIZE)
+    [I, J]=meshgrid(ar(0,1,1./res[1])*SIZE, ar(0,1,1./res[0])*SIZE)
+
+    aspect = res[0] / res[1]
 
     for t in range(T):
         for i in range(n):
-            A[t]+= exp(-(  ((I-X[t,i,0])**2+(J-X[t,i,1])**2)/(r[i]**2)  )**4    )
+            A[t]+= exp(-(  ((I-X[t,i,0])**2+((J-X[t,i,1])*aspect)**2)/(r[i]**2)  )**4    )
             
         A[t][A[t]>1]=1
     return A
 
 def bounce_mat(res, n=2, T=128, r =None):
-    if r==None: r=array([1.2]*n)
-    x = bounce_n(T,n,r);
+    if r is None: r=array([1.2]*n)
+    x = bounce_n(T,n,r, res=res);
     A = matricize(x,res,r)
     return A
 
 def bounce_vec(res, n=2, T=128, r =None, m =None):
-    if r==None: r=array([1.2]*n)
-    x = bounce_n(T,n,r,m);
+    if r is None: r=array([1.2]*n)
+    x = bounce_n(T,n,r,m, res=res);
     V = matricize(x,res,r)
-    return V.reshape(T, res**2)  
+    return V.reshape(T, res[0], res[1])
 
 # make sure you have this folder
 logdir = './sample'
-def show_sample(V):
+def show_sample(V, res):
     T   = len(V)
-    res = int(sqrt(shape(V)[1]))
-    for t in range(T):
-        plt.imshow(V[t].reshape(res,res),cmap=matplotlib.cm.Greys_r) 
+    # res = int(sqrt(shape(V)[1]))
+    for t in tqdm.trange(T):
+        plt.imshow(V[t].reshape(res[0],res[1]),cmap=matplotlib.cm.Greys_r) 
         # Save it
         fname = logdir+'/'+str(t)+'.png'
-        plt.savefig(fname)      
+        plt.savefig(fname)
         
 if __name__ == "__main__":
-    res=30
+    res=(32, 64)
+    n = 3
+    r = array([0.6]*n)
+    #r = array([1.0]*n)
     T=100
+    #T=40
+    #N=10
     N=4000
-    dat=empty((N), dtype=object)
-    for i in xrange(N):
-        dat[i]=bounce_vec(res=res, n=3, T=100)
+    dat=[]
+    for i in tqdm.trange(N):
+        dat.append(bounce_vec(res=res, n=n, T=T, r=r))
     data={}
-    data['Data']=dat
-    scipy.io.savemat('bouncing_balls_training_data.mat',data)
+    data['train']=array(dat)
     
     N=200
-    dat=empty((N), dtype=object)
-    for i in xrange(N):
-        dat[i]=bounce_vec(res=res, n=3, T=100)
-    data={}
-    data['Data']=dat
-    scipy.io.savemat('bouncing_balls_testing_data.mat',data)    
+    dat=[]
+    for i in tqdm.trange(N):
+        dat.append(bounce_vec(res=res, n=n, T=T, r=r))
+    data['test']=array(dat)
+
+    print("writing bouncing_balls_train.npy...")
+    save("bouncing_balls_train.npy", data['train'])
+
+    print("writing bouncing_balls_test.npy...")
+    save("bouncing_balls_test.npy", data['test'])
+
+    #breakpoint()
     
     # show one video
-    # show_sample(dat[1])
+    # show_sample(dat[1], res)
     # ffmpeg -start_number 0 -i %d.png -c:v libx264 -pix_fmt yuv420p -r 30 sample.mp4
 
         
